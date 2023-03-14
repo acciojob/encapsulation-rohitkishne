b05cd7481daf7daf81b955f815917fb441deb8ac
rohitkishne <rohitkishne3@gmail.com>
2023-03-14 17:13:43 +0530

    Modified Assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,8 +5,8 @@ public class Main {
   {
       RWOnly rw = new RWOnly();
 
-      rw.name = "rohit";
-      System.out.println(rw.name);
+//      rw.name = "rohit";
+//      System.out.println(rw.name);
       //Error : java: name has private access in com.driver.RWOnly;
 
       rw.setName("rohit");
