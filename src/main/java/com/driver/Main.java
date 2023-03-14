5463ac341f61499a0c3de5d2ef698ce08558113a
rohitkishne <rohitkishne3@gmail.com>
2023-03-14 17:11:30 +0530

    Assignment Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args)
+  {
+      RWOnly rw = new RWOnly();
+
+      rw.name = "rohit";
+      System.out.println(rw.name);
+      //Error : java: name has private access in com.driver.RWOnly;
+
+      rw.setName("rohit");
+      rw.getName();
+
+
+  }
 }
